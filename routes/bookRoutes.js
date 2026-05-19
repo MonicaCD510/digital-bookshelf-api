c9772503e7d0f6e7d721cbf0397e081f4e9da305
Monica Davila <monica.davila510@gmail.com>
2026-05-18 22:12:32 -0500

    set up book routes router

---
+++ routes/bookRoutes.js
@@ -0,0 +1,9 @@
+// dependencies
+const express = require("express");
+const Book = require("../models/Book");
+
+// router configuration
+const router = express.Router();
+
+// export router
+module.exports = router;
